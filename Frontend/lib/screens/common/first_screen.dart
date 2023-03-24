cabe9aa6a444875a12e9429f2d1f6d0ac23551a6
bLitz <ihassansajjad@gmail.com>
2023-03-24 23:34:49 +0500

    added the asking page (tutor or student)

--- Frontend/lib/screens/common/first_screen.dart
+++ Frontend/lib/screens/common/first_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../welcome_screen.dart';
 import '../student/student_sign_up.dart';
+import 'asking_page.dart';
 import 'log_in.dart';
 
 class FirstScreen extends StatelessWidget {
@@ -10,109 +11,114 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            SizedBox(height: 141),
-            Text.rich(
-              TextSpan(
+            Flexible(
+              flex: 3,
+              child: Container(
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.only(top: 141),
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Level up your\nlearning\nFind your ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 40,
+                          fontFamily: 'JakartaSans',
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'tutor',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 40,
+                          fontFamily: 'JakartaSans',
+                          color: Color(0xFFF2FF53),
+                        ),
+                      ),
+                      TextSpan(
+                        text: '\ntoday!',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 40,
+                          fontFamily: 'JakartaSans',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Column(
                 children: [
-                  TextSpan(
-                    text: 'Level up your\nlearning\nFind your ',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 40,
-                      fontFamily: 'JakartaSans',
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                      backgroundColor:
+                          MaterialStateProperty.all(Color(0xFF583BE8)),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => AskingPage()),
+                      );
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16.0,
+                        horizontal: 32.0,
+                      ),
+                      child: Text(
+                        'Get Started',
+                        style: TextStyle(
+                          fontFamily: 'JakartaSans',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
                   ),
-                  TextSpan(
-                    text: 'tutor',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 40,
-                      fontFamily: 'JakartaSans',
-                      color: Color(0xFFF2FF53),
-                    ),
+                  SizedBox(
+                    height: 28,
                   ),
-                  TextSpan(
-                    text: '\ntoday!',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 40,
-                      fontFamily: 'JakartaSans',
+                  RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Already have an account?',
+                          style: TextStyle(
+                            color: Color(0xFFF2FF53),
+                            fontSize: 15,
+                            fontFamily: 'JakartaSans',
+                            fontWeight: FontWeight.w400,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => WelcomeScreen()),
+                              );
+                            },
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 220),
-            SizedBox(
-              height: 52,
-              width: 247,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    )),
-                    backgroundColor:
-                        MaterialStatePropertyAll(Color(0xFF583BE8))),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => StudentSignUpScreen()),
-                  );
-                },
-                child: Text(
-                  'Get Started',
-                  style: TextStyle(
-                    fontFamily: 'JakartaSans',
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 28,
-            ),
-            RichText(
-                text: TextSpan(children: <TextSpan>[
-              TextSpan(
-                  text: 'Already have an account?',
-                  style: TextStyle(
-                    color: Color(0xFFF2FF53),
-                    fontSize: 15,
-                    fontFamily: 'JakartaSans',
-                    fontWeight: FontWeight.w400,
-                  ),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => WelcomeScreen()),
-                      );
-                    })
-            ]))
           ],
         ),
-      ),
-    );
-  }
-}
-
-class LoginScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: Text('Login Page'),
       ),
     );
   }
