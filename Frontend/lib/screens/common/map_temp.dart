b671138c29d82411b446fc10a437d366ec2688da
Mustafa Amir Khokhar <mustafakhokhar26@gmail.com>
2023-03-26 16:05:36 +0500

    update

--- Frontend/lib/screens/common/map_temp.dart
+++ Frontend/lib/screens/common/map_temp.dart
@@ -1,10 +1,6 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -15,32 +11,34 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
-  String map_theme = '';
+  String maptheme = '';
   final LatLng _center = const LatLng(31.4707, 74.4098);
 
-  void initState(){
-  super.initState();
-  DefaultAssetBundle.of(context).load('lib/assets/maptheme.json').then((value) {
-    map_theme = utf8.decode(value.buffer.asUint8List());
-  });
-}
+  @override
+  void initState() {
+    super.initState();
+    DefaultAssetBundle.of(context)
+        .load('lib/assets/maptheme.json')
+        .then((value) {
+      maptheme = utf8.decode(value.buffer.asUint8List());
+    });
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
-    controller.setMapStyle(map_theme);
+    controller.setMapStyle(maptheme);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 16.0,
-          ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 16.0,
         ),
-      );
-
+      ),
+    );
   }
 }
