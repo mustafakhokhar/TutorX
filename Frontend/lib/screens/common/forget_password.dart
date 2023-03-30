00771433517dc11f73962d869df65f1c7a7d2cef
ABilalSajid <ahmadbilalsajid@gmail.com>
2023-03-30 09:43:54 +0500

    Forgot Password Page/Screen

--- Frontend/lib/screens/common/forget_password.dart
+++ Frontend/lib/screens/common/forget_password.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:tutorx/utils/auth.dart';
-import 'package:tutorx/screens/common/log_in.dart';
 
 class ForgetPasswordScreen extends StatefulWidget {
   @override
@@ -9,51 +6,107 @@ class ForgetPasswordScreen extends StatefulWidget {
 }
 
 class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
-
   final _emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Forget Password'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.black,
+            ),
+            Positioned(
+              bottom: -10,
+              left: 16,
+              right: 16,
+              height: MediaQuery.of(context).size.height * 0.8,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xff583BE8),
+                      Color(0xff583BE8).withOpacity(0),
+                    ],
+                    stops: [0.2552, 1],
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(45),
+                    topRight: Radius.circular(45),
+                  ),
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () async {
-                  Authentication.forgetPassword(
-                      context: context, email: _emailController.text);
-
-                  Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => LoginPage()
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'Reset Password',
+                          style: TextStyle(
+                            color: Color(0xFFF2FF53),
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'JakartaSans',
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                      );
-                  
-                },
-                child:
-                    Text('Submit'),
+                        SizedBox(height: 16),
+                        Container(
+                          alignment: Alignment.center,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 160, bottom: 20),
+                            child: TextFormField(
+                              controller: _emailController,
+                              decoration: InputDecoration(
+                                labelText: 'Enter Email',
+                                labelStyle: TextStyle(
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontFamily: 'JakartaSans',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        SizedBox(
+                          width: 80,
+                          height: 70,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // TODO: Implement reset password logic
+                            },
+                            child: Text(
+                              'CONFIRM',
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontFamily: 'JakartaSans',
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.black,
+                              padding: EdgeInsets.only(top: 10, bottom: 10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
