29eca65ade5d355bb4b45b6701acbe359544af7a
ABilalSajid <ahmadbilalsajid@gmail.com>
2023-04-07 07:05:14 +0500

    Successful student sign in  screen dynamic resized

--- Frontend/lib/screens/student/student_successful_sign_in.dart
+++ Frontend/lib/screens/student/student_successful_sign_in.dart
@@ -9,108 +9,109 @@ class StudentSignUpSuccessful extends StatefulWidget {
   State<StudentSignUpSuccessful> createState() =>  StudentSignUpSuccessfulState();
 }
 
-class  StudentSignUpSuccessfulState extends State<StudentSignUpSuccessful> {
+class StudentSignUpSuccessfulState extends State<StudentSignUpSuccessful> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                hexStringToColor("583BE8"),
-                hexStringToColor("312181"),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 100.0,bottom: 10.0),
-                  child: Container(
-                    width: 175.0,
-                    height: 175.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                    ),
-                    child: Icon(
-                      Icons.check,
-                      color: Color.fromARGB(255, 7, 6, 6),
-                      size: 120.0,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 50.0),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 45.0),
-                  child: Text(
-                    'Sign Up Successful!',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 32,
-                      fontFamily: 'JakartaSans',
-                      fontWeight: FontWeight.w800,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 70.0,vertical: 10.0),
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Congratulations! You are now part of our learning community. Let's find the right tutor for you and start achieving your goals!",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 15,
-                      fontFamily: 'JakartaSans',
-                      fontWeight: FontWeight.w800,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                SizedBox(height: 50.0),
-                Padding(
-                  padding: EdgeInsets.only(top: 50.0),
-                  child: SizedBox(
-                    height: 60, // Set button height to 80
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.black,
-                        onPrimary: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        // Set minWidth to 150 and height to 80
-                        minimumSize: Size(300, 80),
-                      ),
-                      onPressed: () {
-                        // Add your onPressed logic here
-                      },
-                      child: Text(
-                        'CONTINUE',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontFamily: 'JakartaSans',
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+      body: Container(
+        width: screenWidth,
+        height: screenHeight,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              hexStringToColor("583BE8"),
+              hexStringToColor("312181"),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
         ),
-      );
-
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: screenHeight * 0.15, bottom: screenHeight * 0.02),
+                child: Container(
+                  width: screenWidth * 0.45,
+                  height: screenWidth * 0.45,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Icon(
+                    Icons.check,
+                    color: Color.fromARGB(255, 7, 6, 6),
+                    size: screenWidth * 0.28,
+                  ),
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.05),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.1),
+                child: Text(
+                  'Sign Up Successful!',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: screenWidth * 0.08,
+                    fontFamily: 'JakartaSans',
+                    fontWeight: FontWeight.w800,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.002),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.15, vertical: screenHeight * 0.01),
+                alignment: Alignment.center,
+                child: Text(
+                  "Congratulations! You are now part of our learning community. Let's find the right tutor for you and start achieving your goals!",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: screenWidth * 0.035,
+                    fontFamily: 'JakartaSans',
+                    fontWeight: FontWeight.w800,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.05),
+              Padding(
+                padding: EdgeInsets.only(top: screenHeight * 0.05),
+                child: SizedBox(
+                  height: screenHeight * 0.1,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black,
+                      onPrimary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(screenHeight * 0.05),
+                      ),
+                      minimumSize: Size(screenWidth * 0.6, screenHeight * 0.1),
+                    ),
+                    onPressed: () {
+                      // Add your onPressed logic here
+                    },
+                    child: Text(
+                      'CONTINUE',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.045,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: 'JakartaSans',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   Color hexStringToColor(String hexString) {
